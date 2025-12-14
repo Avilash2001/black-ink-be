9fa992c9c75e443659a7949ae63dfecbdb2f4e13
Avilash Ghosh <58250550+Avilash2001@users.noreply.github.com>
2025-12-14 14:34:19 +0530

    remove cors

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({
-    origin: '*',
-    credentials: true,
-  });
   await app.listen(3000);
 }
 bootstrap();
